b0a10ae774b135ba2c0e52393318812a9b8cdbbe
takanbi <tunji.akanbi@century.edu>
2022-11-09 10:53:31 -0600

    all files updated

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    // $("div").hide();
+    // $("p").hide();
+    // let windowWidth = window.innerWidth;
+    // let windowHeight = window.innerHeight;
+    // console.log("This is your browser window width: " + windowWidth);
+    // console.log("This is your browser window height: " + windowHeight);
+    // if(windowWidth < 720) {
+    //     $("p").hide();
+    // }
+    // $("div > p").hide();
+    $("button#myBtn").click(function(){
+        $("p#myParagraph").hide();
+    });
+});
